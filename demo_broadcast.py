e0d6bd8d9dcb52f6f67e28ea646d332f4b453585
GitHub-ZhangYuhao <zyh924429588@gmail.com>
2025-03-22 15:39:57 +0800

    torch 广播机制

---
+++ demo_broadcast.py
@@ -0,0 +1,22 @@
+﻿import torch
+
+#广播机制可以用在加减乘除的数学运算
+a = torch.rand(2,3)
+b = torch.rand(3)
+c = a+b
+print(a)
+print(b)
+print(c)
+print(c.shape)
+# a, 2*3
+# b, 1*3
+# c, 2*3
+
+a = torch.rand(2, 1, 1, 2)
+b = torch.rand(4, 3, 2)
+c = a-b
+print(a)
+print(b)
+print(c)
+print(c.shape)
+#c.Shape = (2, 4, 3, 2)
